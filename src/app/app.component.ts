00bfcb83974caf3c39a8143c23260b824787b76c
FabianRausch <fabianr95.fr@gmail.com>
2020-05-21 01:18:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  nombre    : string   = 'Capián América';
+  nombre2   : string   = 'fAbiaN raUScH';
+  personajes: string[] = ['Iroman','Spiderman','Thor','Loki','Groot'];
+  PI        : number   = Math.PI;
+  porcentaje: number   = 0.234;
+  salario   : number   = 1234.5;
+  fecha     : Date     = new Date();
+  activar   : boolean  = true;
+  idioma    : string   = 'es';
+  videoUrl  : string   = 'https://www.youtube.com/embed/q7MBx-HFMr4';
+
+  valorPromesa = new Promise<string>((resolve) => {
+
+  setTimeout(() => {
+    resolve('llego la data');
+  }, 4500);
+
+  })
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
